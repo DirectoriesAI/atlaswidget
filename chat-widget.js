2f9c39a019f98574a46c92062029b57edaaf3350
DirectoriesAdmin <admin@directories.ai>
2023-12-11 15:43:18 -0500

    prod

--- chat-widget.js
+++ chat-widget.js
@@ -1,5 +1,14 @@
-(function() {
+(function(options) {
   document.head.insertAdjacentHTML('beforeend', '<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.16/tailwind.min.css" rel="stylesheet">');
+
+  // Use the assistantId, atlasApiKey, title, mainColor, and secondaryColor from the options
+  const assistantId = options.assistantId;
+  const atlasApiKey = options.atlasApiKey;
+  const title = options.title || 'Atlas AI Assistant';
+  const mainColor = options.mainColor || '#1F2937'; // Default to Tailwind's gray-800
+  const secondaryColor = options.secondaryColor || '#FFFFFF'; // Default to white
+  const logoUrl = options.logoUrl || 'atlas-logo.jpeg'; // Default to 'atlas-logo.jpeg'
+  const initialMessage = options.initialMessage || 'Hello from Atlas';
 
   // Inject the CSS
   const style = document.createElement('style');
@@ -32,6 +41,15 @@
       border-radius: 0;
     }
   }
+  #chat-header {
+    background-color: ${mainColor};
+  }
+  #chat-submit {
+    background-color: ${mainColor};
+  }
+  #chat-bubble {
+    background-color: ${secondaryColor};
+  }
   `;
 
   document.head.appendChild(style);
@@ -43,30 +61,33 @@
   
   // Inject the HTML
   chatWidgetContainer.innerHTML = `
-    <div id="chat-bubble" class="w-16 h-16 bg-gray-800 rounded-full flex items-center justify-center cursor-pointer text-3xl">
-      <img src="atlas-logo.jpeg" alt="Atlas Logo" class="w-10 h-10">
+    <div id="chat-bubble" class="w-16 h-16 rounded-full flex items-center justify-center cursor-pointer text-3xl">
+      <img src="${logoUrl}" alt="Atlas Logo" class="w-16 h-16 rounded-full">
     </div>
     <div id="chat-popup" class="hidden absolute bottom-20 right-0 w-96 bg-white rounded-md shadow-md flex flex-col transition-all text-sm">
-      <div id="chat-header" class="flex justify-between items-center p-4 bg-gray-800 text-white rounded-t-md">
-        <h3 class="m-0 text-lg">Atlas Assistant</h3>
+      <div id="chat-header" class="flex justify-between items-center p-4 text-white rounded-t-md">
+        <h3 class="m-0 text-lg">${title}</h3>
         <button id="close-popup" class="bg-transparent border-none text-white cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
       </div>
-      <div id="chat-messages" class="flex-1 p-4 overflow-y-auto"></div>
+      <div id="chat-messages" class="flex-1 p-4 overflow-y-auto">
+        ${initialMessage ? `<div class="p-4 text-black">${initialMessage}</div>` : ''}
+      </div>
       <div id="chat-input-container" class="p-4 border-t border-gray-200">
         <div class="flex space-x-4 items-center">
           <input type="text" id="chat-input" class="flex-1 border border-gray-300 rounded-md px-4 py-2 outline-none w-3/4" placeholder="Type your message...">
-          <button id="chat-submit" class="bg-gray-800 text-white rounded-md px-4 py-2 cursor-pointer">Send</button>
+          <button id="chat-submit" class="text-white rounded-md px-4 py-2 cursor-pointer">Send</button>
         </div>
         <div class="flex text-center text-xs pt-4">
-          <span class="flex-1">Built by <a href="https://twitter.com/directoriesai" target="_blank" class="text-indigo-600">@directoriesai</a></span>
+          <span class="flex-1">Built by <a href="https://twitter.com/aidirectories" target="_blank" class="text-indigo-600">@directoriesai</a></span>
         </div>
       </div>
     </div>
   `;
+})(window.chatWidgetOptions || {});
 
   // Add event listeners
   const chatInput = document.getElementById('chat-input');
@@ -129,9 +150,26 @@
     chatInput.value = '';
   
     // Reply to the user
-    setTimeout(function() {
-      reply('Hello! This is a sample reply.');
-    }, 1000);
+    fetch('https://run.directories.ai/api/atlas/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${atlasApiKey}`
+      },
+      body: JSON.stringify({ message: message, assistantId: assistantId })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data && data.reply) {
+        reply(data.reply);
+      }
+    })
+    .catch(error => {
+      console.error('Error during Atlas API call:', error);
+      setTimeout(() => {
+        reply('Please ensure you have included your Atlas API key and assistant id.');
+      }, 5000);
+    });
   }
   
   function reply(message) {
@@ -147,4 +185,4 @@
     chatMessages.scrollTop = chatMessages.scrollHeight;
   }
   
-})();
+;
